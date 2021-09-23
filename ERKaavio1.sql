aa319fd246a891779c022236b22082ffab9a8255
TinoCederholm <42689@edu.raseko.fi>
2021-09-23 10:08:08 +0300

    muutoksia

--- ERKaavio1.sql
+++ ERKaavio1.sql
@@ -3,97 +3,73 @@
 -- FLOAT -> REAL
 
 CREATE TABLE tuoteryhma (
-                tuoteryhma TEXT NOT NULL,
-                CONSTRAINT tuoteryhma_pk PRIMARY KEY (tuoteryhma)
+                tuoteryhma TEXT PRIMARY KEY
 );
 
 
 CREATE TABLE tuote (
-                yksilointikoodi INTEGER NOT NULL,
+                yksilointikoodi INTEGER PRIMARY KEY,
                 tuoteryhma TEXT NOT NULL,
                 nimike TEXT NOT NULL,
                 merkki TEXT NOT NULL,
                 malli TEXT NOT NULL,
                 paivavuokra REAL NOT NULL,
-                CONSTRAINT tuote_pk PRIMARY KEY (yksilointikoodi)
+                FOREIGN KEY (tuoteryhma) REFERENCES tuoteryhma (tuoteryhma)
 );
 
 
 CREATE TABLE yhteystyyppi (
-                yhteystyyppi TEXT NOT NULL,
-                CONSTRAINT yhteystyyppi_pk PRIMARY KEY (yhteystyyppi)
+                yhteystyyppi TEXT PRIMARY KEY
 );
 -- COMMENT ON COLUMN yhteystyyppi.yhteystyyppi IS 'Taulu on luotu vain k�ytt�liittym�� varten. Se est�� k�ytt�j�� sy�tt�m�st� Yhteyshenkil�-tauluun muita kuin ennalta m��r�ttyj� arvoja.';
 
 
 CREATE TABLE asiakas (
-                asiakasnumero INTEGER NOT NULL,
+                asiakasnumero INTEGER PRIMARY KEY,
                 yritys TEXT NOT NULL,
                 laskutusosoite TEXT NOT NULL,
                 postinumero TEXT NOT NULL,
                 postitoimipaikka TEXT NOT NULL,
-                CONSTRAINT asiakas_pk PRIMARY KEY (asiakasnumero)
+                
 );
 -- COMMENT ON TABLE asiakas IS 'Asiakkaan perustiedot';
 -- COMMENT ON COLUMN asiakas.laskutusosoite IS 'Yrityksen laskujen katu- tai PL-osoite';
 
 
-CREATE TABLE vuokraus (
-                tapahtumaid INTEGER NOT NULL,
-                asiakasnumero INTEGER NOT NULL,
-                yksilointikoodi INTEGER NOT NULL,
-                alkamispaiva DATE NOT NULL,
-                paattymispaiva DATE NOT NULL,
-                CONSTRAINT vuokraus_pk PRIMARY KEY (tapahtumaid)
-);
--- COMMENT ON COLUMN vuokraus.tapahtumaid IS 'Dummy id tulevia k�ytt�tarkoituksia varten';
+
 
 
 CREATE TABLE yhteyshenkilo (
-                yhteyshenkilo_id INTEGER NOT NULL,
+                yhteyshenkilo_id INTEGER PRIMARY KEY,
                 yhteystyyppi TEXT NOT NULL,
                 etunimi TEXT NOT NULL,
                 sukunimi TEXT NOT NULL,
                 puhelin TEXT,
                 sahkoposti TEXT NOT NULL,
                 asiakasnumero INTEGER NOT NULL,
-                CONSTRAINT yhteyshenkilo_pk PRIMARY KEY (yhteyshenkilo_id)
+                FOREIGN KEY (yhteystyyppi) REFERENCES yhteystyyppi (yhteystyyppi),
+                FOREIGN KEY (asiakasnumero) REFERENCES asiakas (asiakasnumero)
+
 );
 -- COMMENT ON COLUMN yhteyshenkilo.yhteyshenkilo_id IS 'Dummy perusavain. Tulevaisuuden tarpeita varten';
 -- COMMENT ON COLUMN yhteyshenkilo.yhteystyyppi IS 'Taulu on luotu vain k�ytt�liittym�� varten. Se est�� k�ytt�j�� sy�tt�m�st� Yhteyshenkil�-tauluun muita kuin ennalta m��r�ttyj� arvoja.';
 
+CREATE TABLE vuokraus (
+                tapahtumaid INTEGER PRIMARY KEY,
+                asiakasnumero INTEGER NOT NULL,
+                yksilointikoodi INTEGER NOT NULL,
+                alkamispaiva DATE NOT NULL,
+                paattymispaiva DATE NOT NULL,
+                FOREIGN KEY (asiakasnumero) REFERENCES asiakas (asiakasnumero),
+                FOREIGN KEY (yksilointikoodi) REFERENCES tuote (yksilointikoodi)
+);
+-- COMMENT ON COLUMN vuokraus.tapahtumaid IS 'Dummy id tulevia k�ytt�tarkoituksia varten';
 
-ALTER TABLE tuote ADD CONSTRAINT tuoteryhma_tuote_fk
-FOREIGN KEY (tuoteryhma)
-REFERENCES tuoteryhma (tuoteryhma)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
 
-ALTER TABLE vuokraus ADD CONSTRAINT tuote_vuokraus_fk
-FOREIGN KEY (yksilointikoodi)
-REFERENCES tuote (yksilointikoodi)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
 
-ALTER TABLE yhteyshenkilo ADD CONSTRAINT yhteystyyppi_yhteyshenkilo_fk
-FOREIGN KEY (yhteystyyppi)
-REFERENCES yhteystyyppi (yhteystyyppi)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
 
-ALTER TABLE yhteyshenkilo ADD CONSTRAINT asiakas_yhteyshenkilo_fk
-FOREIGN KEY (asiakasnumero)
-REFERENCES asiakas (asiakasnumero)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
 
-ALTER TABLE vuokraus ADD CONSTRAINT asiakas_vuokraus_fk
-FOREIGN KEY (asiakasnumero)
-REFERENCES asiakas (asiakasnumero)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
+
+
+
+
